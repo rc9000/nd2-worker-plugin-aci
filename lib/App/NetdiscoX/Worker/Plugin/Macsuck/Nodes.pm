9f867b9ef2b1e7837ae531548fbf1c4ad190cba9
Christian Ramseyer <ramseyer@netnea.com>
2019-09-16 17:59:11 +0200

    Implement node filter same as SNMP-based Macsuck::Nodes.pm
     * skip macs on uplink
     * skip devices/ports not known from discovery

--- lib/App/NetdiscoX/Worker/Plugin/Macsuck/Nodes.pm
+++ lib/App/NetdiscoX/Worker/Plugin/Macsuck/Nodes.pm
@@ -3,6 +3,7 @@ package App::NetdiscoX::Worker::Plugin::Macsuck::Nodes;
 
 use Dancer ':syntax';
 use App::Netdisco::Worker::Plugin;
+use App::Netdisco::Util::Device qw/get_device/;
 use aliased 'App::Netdisco::Worker::Status';
 
 use Dancer::Plugin::DBIC 'schema';
@@ -33,13 +34,50 @@ register_worker({ phase => 'main', driver => 'netconf' }, sub {
   my $mac_arp_info = $aci->fetch_mac_arp_info();
   my $now = 'to_timestamp('. (join '.', gettimeofday) .')';
   my $n = 0;
+  my $fab_devices = {};
+  my $fab_device_ports = {};
 
   info sprintf ' [%s] cisco aci macsuck - updating node table', $device->ip;
   foreach my $entry (@{$mac_arp_info->{nodes}}){
-    App::Netdisco::Worker::Plugin::Macsuck::Nodes::store_node(
-      $entry->{switch}, $entry->{vlan}, $entry->{port}, $entry->{mac}, $now
-    );
-    $n++ 
+
+    unless ($fab_devices->{$entry->{switch}}){
+      $fab_devices->{$entry->{switch}} = get_device($entry->{switch}) unless $fab_devices->{$entry->{switch}};
+    }
+
+    unless ($fab_device_ports->{$entry->{switch}."".$entry->{port}}){
+      my $fab_dps = {map {($_->port => $_)}
+                          $fab_devices->{$entry->{switch}}->ports(undef, {prefetch => {neighbor_alias => 'device'}})->all};
+
+      $fab_device_ports->{$entry->{switch}."".$entry->{port}} = $fab_dps->{$entry->{port}};
+      #debug sprintf ' [%s] cisco aci macsuck - %s fabric switch %s port %s ->port read from DB',  
+      #  $device->ip, $entry->{mac}, $entry->{switch}, $entry->{port};
+
+    }else{
+
+      #debug sprintf ' [%s] cisco aci macsuck - %s fabric switch %s port %s ->port read from cache',  
+      #  $device->ip, $entry->{mac}, $entry->{switch}, $entry->{port};
+
+    }
+
+    my $fab_p = $fab_device_ports->{$entry->{switch}."".$entry->{port}};
+
+    if (!$fab_p){
+      debug sprintf ' [%s] cisco aci macsuck - %s fabric switch %s port %s is unknown to netdisco, skipped', 
+        $device->ip, $entry->{mac}, $entry->{switch}, $entry->{port};
+    
+    } elsif ($fab_p->is_uplink){
+      debug sprintf ' [%s] cisco aci macsuck - %s fabric switch %s port %s is an uplink, skipped', 
+        $device->ip, $entry->{mac}, $entry->{switch}, $fab_p->port;
+
+    }else{
+      debug sprintf ' [%s] cisco aci macsuck - %s fabric switch %s port %s stored',  
+        $device->ip, $entry->{mac}, $entry->{switch}, $fab_p->port;
+
+      App::Netdisco::Worker::Plugin::Macsuck::Nodes::store_node(
+        $entry->{switch}, $entry->{vlan}, $entry->{port}, $entry->{mac}, $now
+      );
+      $n++ 
+    }
   }
 
   info sprintf ' [%s] cisco aci macsuck - stored %s node entries', $device->ip, $n;
